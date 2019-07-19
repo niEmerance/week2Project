90dac95740be1dad775fc19a1ad7a7994ec2dd77
niEmerance <niyemera6@gmail.com>
2019-07-19 12:17:47 +0200

    Rename index.html to main.html

---
+++ index.js
@@ -0,0 +1,26 @@
+function getInput(){
+    function century(){
+        var ln=document.getElementById("century").value;
+        date();
+    }
+    
+    function date(){
+        var bd=document.getElementById("birthDate").value;
+
+    var bdArray=bd.split("-");
+    var YY=parseInt(bdArray[0]); 
+        CC=19;
+        MM=parseInt(bdArray[1]);
+        DD=parseInt(bdArray[2]);
+
+      var d=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+
+      alert(ln + Math.round(d));
+      selectGender();
+    }
+    function selectGender(){
+    var gd=document.getElementById("gender").value;
+}
+}
+
+
